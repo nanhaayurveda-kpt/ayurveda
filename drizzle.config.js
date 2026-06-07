0af1ff5b5e62d0466a42d13f3bb42d0204cf8794
kp1153 <kamta_tiwari@yahoo.com>
2026-06-07 20:08:28 +0530

    force deploy

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   schema: "./lib/schema.js",
   dialect: "turso",
   dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN,
+    url: "libsql://ayurveda-ayurveda.aws-ap-south-1.turso.io",
+    authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODA4MTM3NzYsImlkIjoiMDE5ZWEwYzUtMTAwMS03YThjLWE5MjQtYmY3YWQ0YjZiMmFmIiwicmlkIjoiMGU2NDJlNTUtYTNmNS00MDFiLWE4YmQtYzBiOTc5MjExOGViIn0.c4J7GWtu6JJWrv-WQrFhigiYh4EAYlrEtAsSDjwa9ZiegYgiOe45Ylq3kIMWOjytW-fr39YUrl2gt4GyKFevAw",
   },
 });
